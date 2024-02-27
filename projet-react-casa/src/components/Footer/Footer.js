5b62fc1ebbcf2d752b3cdca23d66f7fcaaef43da
zaidi ibtissem <awessale@gmail.com>
2024-02-27 23:18:40 +0100

    Correction media queries

--- projet-react-casa/src/components/Footer/Footer.js
+++ projet-react-casa/src/components/Footer/Footer.js
@@ -3,12 +3,10 @@ import logo from "../../assets/logo-footer.svg";
 import "./Footer.scss";
 const Footer = () => {
   return (
-    <footer>
-      <div className="container">
-        <img src={logo} alt="logo" />
-        <div className="copyright"> © 2020 Kasa. All rights reserved</div>
-      </div>
-    </footer>
+    <div className="footer">
+      <img src={logo} alt="logo" />
+      <div className="copyright"> © 2020 Kasa. All rights reserved</div>
+    </div>
   );
 };
 
--- projet-react-casa/src/components/Header/Header.js
+++ projet-react-casa/src/components/Header/Header.js
@@ -5,29 +5,27 @@ import "./Header.scss";
 
 const Header = () => {
   return (
-    <header>
-      <div className="container">
-        <img src={logo} className="Logo" alt="logo" />
-        <ul>
-          <li>
-            <NavLink
-              to={"/"}
-              className={(nav) => (nav.isActive ? "nav-active" : "")}
-            >
-              Accueil
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to={"/about"}
-              className={(nav) => (nav.isActive ? "nav-active" : "")}
-            >
-              A propos
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-    </header>
+    <div className="container">
+      <img src={logo} className="Logo" alt="logo" />
+      <ul>
+        <li>
+          <NavLink
+            to={"/"}
+            className={(nav) => (nav.isActive ? "nav-active" : "")}
+          >
+            Accueil
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/about"}
+            className={(nav) => (nav.isActive ? "nav-active" : "")}
+          >
+            A propos
+          </NavLink>
+        </li>
+      </ul>
+    </div>
   );
 };
 export default Header;
